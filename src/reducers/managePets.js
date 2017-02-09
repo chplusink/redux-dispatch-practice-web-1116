9d5ab21cf441149351f9c3fbe1d2c8a5b0b5e1d9
Vic Lam <lam.vic.k@gmail.com>
2017-02-08 19:14:00 -0500

    Add reducer, dispatcher,  and render functions

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,28 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state = { pets: [] }, action){
+  switch (action.type) {
+    case 'ADD_PET':
+      return { ...state, pets: [...state.pets, action.payload] }
+    case 'REMOVE_PET':
+      let newPets = state.pets.filter(function(pet) {
+        return pet.id !== action.payload
+      })
+      return { ...state, pets: newPets }
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch(action){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
-
+  let petListItems = state.pets.map(function(pet) {
+    return `<li>${pet.name}</li>`
+  }).join('')
+  document.getElementById('container').innerHtml = `<ul>${petListItems}</ul>`
 }
